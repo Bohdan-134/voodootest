3b99110bf2732c7437e1b15b6b0a699c80ddd35b
kBohdan <bkachmar1@gmail.com>
2023-05-28 12:03:38 +0300

    add accordion and style for accordion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -62,3 +62,15 @@ window.onclick = function(event) {
 signInBtn.onclick = function() {
     createModal();
 };
+
+
+// ACCORDEON
+function toggleAccordion(header) {
+    var content = header.nextElementSibling;
+    header.classList.toggle('active');
+    if (header.classList.contains('active')) {
+        content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+        content.style.maxHeight = '0';
+    }
+}
